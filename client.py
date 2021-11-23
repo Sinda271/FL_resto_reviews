026d77ea688d43d05b0241dc2f5d041265cf0857
Sinda Besrour <sbesrour@dom.tti>
2021-11-23 11:15:21 +0100

    first commit

---
+++ client.py
@@ -0,0 +1,62 @@
+import flwr as fl
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+import utils
+import sys
+import warnings
+from fastapi import FastAPI
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+app = FastAPI()
+
+@app.post("/participateFL")
+def listen_and_participate(train_start:int, train_end:int, ipaddress:str ,port:int):
+    # Create LogisticRegression Model
+    model = LogisticRegression(
+        penalty="l2",
+        max_iter=10,  # local epoch
+        warm_start=True,  # prevent refreshing weights when fitting
+    )
+    # Setting initial parameters, akin to model.compile for keras models
+    utils.set_initial_params(model)
+
+
+    # Load dataset
+    df = pd.read_csv('cleaned_dataset.csv')
+    X = df.drop(['Target'], axis=1)
+    y = df['Target']
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0, shuffle=False)
+    x_train, y_train = x_train[train_start:train_end], y_train[train_start:train_end]
+
+
+    # Define Flower client
+    class FlowerClient(fl.client.NumPyClient):
+
+        def get_parameters(self):
+            return utils.get_model_parameters(model)
+
+        def fit(self, parameters, config):
+            utils.set_model_params(model, parameters)
+            model.fit(x_train, y_train)
+            return utils.get_model_parameters(model), len(x_train), {}
+
+        def evaluate(self, parameters, config):
+            utils.set_model_params(model, parameters)
+            loss = log_loss(y_test, model.predict_proba(x_test))
+            accuracy = model.score(x_test, y_test)
+            print("accuracy: ", accuracy)
+            return loss, len(x_test), {"accuracy": accuracy}
+
+
+    # Start Flower client
+    fl.client.start_numpy_client(
+        server_address=ipaddress + ':' + str(port),
+        client=FlowerClient(),
+        grpc_max_message_length=1024 * 1024 * 1024
+    )
+
+
---
+++ server.py
@@ -0,0 +1,112 @@
+import flwr as fl
+import numpy as np
+import os
+from typing import Callable, Dict
+import datetime as dt
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.post("/FLsession")
+def start_fl_session(num_rounds:int,ipaddress:str ,port:int,resume:bool):
+
+    # define date and time to save weights in directories
+    today = dt.datetime.today()
+    session = today.strftime("%d-%m-%Y-%H-%M-%S")
+
+    class SaveModelStrategy(fl.server.strategy.FedAvg):
+        def aggregate_fit(
+                self,
+                rnd,
+                results,
+                failures
+        ):
+            aggregated_weights = super().aggregate_fit(rnd, results, failures)
+            if aggregated_weights is not None:
+
+                # Save aggregated_weights
+                print(f"Saving round {rnd} aggregated_weights...")
+
+                if not os.path.exists(f"Session-{session}"):
+                    os.makedirs(f"Session-{session}")
+                    if rnd < num_rounds:
+                        np.save(f"Session-{session}/round-{rnd}-weights.npy", aggregated_weights)
+                    elif rnd == num_rounds:
+                        np.save(f"Session-{session}/global_session_model.npy", aggregated_weights)
+                else:
+                    if rnd < num_rounds:
+                        np.save(f"Session-{session}/round-{rnd}-weights.npy", aggregated_weights)
+                    elif rnd == num_rounds:
+                        np.save(f"Session-{session}/global_session_model.npy", aggregated_weights)
+
+            return aggregated_weights
+
+
+    # Define batch-size, nb of epochs and verbose for fitting
+    def get_on_fit_config_fn() -> Callable[[int], Dict[str, str]]:
+        """Return a function which returns training configurations."""
+
+        def fit_config(rnd: int) -> Dict[str, str]:
+            config = {
+                "batch_size": 32,
+                "epochs": 50,
+                "verbose": 0,
+            }
+            return config
+
+        return fit_config
+
+
+    # Define hyper-parameters for evaluation
+    def evaluate_config(rnd: int):
+        val_steps = 5 if rnd < 4 else 10
+        return {"val_steps": val_steps, "verbose": 0}
+
+
+    # Create strategy and run server
+    # Load last session weights if they exist
+    sessions = ['no session']
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in dirs:
+            if name.find('Session') != -1:
+                sessions.append(name)
+
+    if os.path.exists(f'{sessions[-1]}/global_session_model.npy'):
+        initial_parameters = np.load(f"{sessions[-1]}/global_session_model.npy", allow_pickle=True)
+        initial_weights = initial_parameters[0]
+    else:
+        initial_weights = None
+
+    if resume:
+        strategy = SaveModelStrategy(
+            fraction_fit=1.0,
+            fraction_eval=1.0,
+            min_fit_clients=2,
+            min_eval_clients=2,
+            min_available_clients=2,
+            initial_parameters=initial_weights,
+            on_fit_config_fn=get_on_fit_config_fn(),
+            on_evaluate_config_fn=evaluate_config,
+        )
+    else:
+        strategy = SaveModelStrategy(
+            fraction_fit=1.0,
+            fraction_eval=1.0,
+            min_fit_clients=2,
+            min_eval_clients=2,
+            min_available_clients=2,
+            initial_parameters=None,
+            on_fit_config_fn=get_on_fit_config_fn(),
+            on_evaluate_config_fn=evaluate_config,
+        )
+
+
+    fl.server.start_server(
+        server_address=ipaddress + ':' + str(port),
+        config={"num_rounds": num_rounds},
+        grpc_max_message_length=1024 * 1024 * 1024,
+        strategy=strategy
+    )
+
+
